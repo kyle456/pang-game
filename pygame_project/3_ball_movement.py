8ebafce4505b452326781b5a0737b233cedab99c
kyle456 <kyle2123@naver.com>
2022-05-07 23:20:05 +0900

    Feat: ball movements

--- pygame_project/3_ball_movement.py
+++ pygame_project/3_ball_movement.py
@@ -54,12 +54,12 @@ ball_speed_y = [-18, -15, -12, -9]  # balloon 1, 2, 3, 4의 최초 스피드
 # 일단 가장 큰 공(balloon 1)에 대한 정보를 딕셔너리 원소로 초기화
 balls = [
     {
-        "pos_x": 50, # 공의 x 좌표
-        "pos_y" : 50, # 공의 y 좌표
-        "image_index" : 0, # balloon 번호
-        "to_x": 3, # 공의 x축 이동
-        "to_y": -6, # 공의 y축 이동
-        "init_speed_y": ball_speed_y[0] # y축 이동 최초 속도
+        "pos_x": 50,  # 공의 x 좌표
+        "pos_y": 50,  # 공의 y 좌표
+        "image_index": 0,  # balloon 번호
+        "to_x": 3,  # 공의 x축 이동
+        "to_y": -6,  # 공의 y축 이동
+        "init_speed_y": ball_speed_y[0],  # y축 이동 최초 속도
     }
 ]
 
@@ -104,6 +104,26 @@ while is_playing:
         if weapon_y_pos > 0
     ]
 
+    for index, ball in enumerate(balls):
+        ball_pos_x = ball["pos_x"]
+        ball_pos_y = ball["pos_y"]
+        ball_image_index = ball["image_index"]
+
+        ball_size = ball_images[ball_image_index].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+
+        # 공이 벽과 바닥에 닿으면 방향을 반대로 전환
+        if ball_pos_x < 0 or ball_pos_x > screen_width - ball_width:
+            ball["to_x"] *= -1
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball["to_y"] = ball["init_speed_y"]
+        else:
+            ball["to_y"] += 0.5  # 위로 진행할 때는 속도를 줄이고, 아래로 진행할 때는 속도를 늘림 (포물선 효과)
+
+        ball["pos_x"] += ball["to_x"]
+        ball["pos_y"] += ball["to_y"]
+
     # 4. 충돌 처리
 
     # 5. 화면에 그리기 (작성 순서에 따라 우선 순위 바뀜)
@@ -111,6 +131,12 @@ while is_playing:
 
     for weapon_x_pos, weapon_y_pos in weapons:
         screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
+    for index, ball in enumerate(balls):
+        ball_pos_x = ball["pos_x"]
+        ball_pos_y = ball["pos_y"]
+        ball_image_index = ball["image_index"]
+        screen.blit(ball_images[ball_image_index], (ball_pos_x, ball_pos_y))
 
     screen.blit(stage, (0, screen_height - stage_height))
     screen.blit(character, (character_x_pos, character_y_pos))
